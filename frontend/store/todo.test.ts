b7ddacb9ef61b87cc241e45200cdbbd243066d9d
EugeneGpil <gpilligrim@gmail.com>
2022-08-01 04:58:28 +0300

    1

--- frontend/store/todo.test.ts
+++ frontend/store/todo.test.ts
@@ -2,8 +2,6 @@ import {describe, test, expect} from 'vitest'
 
 describe('runs', () => {
     test('it works', () => {
-        expect(true).toBe(true)
+        (expect as Vi.ExpectStatic)(true).toBe(true)
     }, 60 * 1000)
 })
-
-//https://www.youtube.com/watch?v=McZWm6Jhk4o
