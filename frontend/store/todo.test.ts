ee0e10ed7b1d605bb589214f55748a5696fceb64
EugeneGpil <gpilligrim@gmail.com>
2022-08-01 05:32:41 +0300

    1

--- frontend/store/todo.test.ts
+++ frontend/store/todo.test.ts
@@ -1,4 +1,17 @@
-import {describe, test, expect} from 'vitest'
+import {setActivePinia, createPinia} from 'pinia'
+import {describe, test, expect, beforeAll} from 'vitest'
+
+beforeAll(() => {
+    setActivePinia(createPinia())
+})
+
+// describe('useTodoStore', () => {
+//     test('creates a store', () => {
+//
+//     })
+// })
+
+
 
 describe('runs', () => {
     test('it works', () => {
